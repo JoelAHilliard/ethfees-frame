1efbb01600408e131bae00aa38c2e860b0728b2e
Joel <hilliardjoel05@gmail.com>
2024-02-04 17:28:26 -0800

    init

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/png', async (req, res) => {
     const svgData = `
         <svg width="600" height="400" xmlns="http://www.w3.org/2000/svg">
             <rect width="100%" height="100%" fill="green"/>
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="white" font-size="30">Hello, SVG!</text>
         </svg>
     `;
 
