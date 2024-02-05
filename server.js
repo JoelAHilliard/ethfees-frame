c5db5f7b00f92301707212930fe98009f347133d
Joel <hilliardjoel05@gmail.com>
2024-02-04 17:24:38 -0800

    init

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/png', async (req, res) => {
     // Manually construct an SVG string
     const svgData = `
         <svg width="600" height="400" xmlns="http://www.w3.org/2000/svg">
-            <rect width="100%" height="100%" fill="red"/>
+            <rect width="100%" height="100%" fill="green"/>
         </svg>
     `;
 
